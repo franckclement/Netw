d0a10dfb10ba88653c71794a36bba39caf042b00
Franck CLEMENT <franck.clmnt@gmail.com>
2019-08-16 08:02:55 -0400

    Initial Commit

---
+++ Net/Net.h
@@ -0,0 +1,19 @@
+//
+//  Net.h
+//  Net
+//
+//  Created by Franck Clement on 16/08/2019.
+//  Copyright © 2019 Franck Clement. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Net.
+FOUNDATION_EXPORT double NetVersionNumber;
+
+//! Project version string for Net.
+FOUNDATION_EXPORT const unsigned char NetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Net/PublicHeader.h>
+
+
